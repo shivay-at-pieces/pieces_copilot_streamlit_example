be299424cdde30ff6e25591abbd7c73dcfb4c82b
Shivay-at-pieces <143828586+shivay-at-pieces@users.noreply.github.com>
2024-08-22 04:20:59 +0530

    Update pieces-bot.py

--- pieces-bot.py
+++ pieces-bot.py
@@ -1,18 +1,13 @@
 import streamlit as st
-from pieces_copilot_sdk import PiecesClient
+# from pieces_copilot_sdk import PiecesClient
+from pieces_os_client.wrapper import PiecesClient
 
 pieces_client = PiecesClient(config={'baseUrl': 'http://localhost:1000'})
-
 
 st.title("Pieces Copilot Streamlit Bot")
 
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "assistant", "content": "Ask me anything - Pieces Copilot"}]
-
-if "conversation_id" not in st.session_state:
-    # Create a new conversation
-    conversation = pieces_client.create_conversation(props={"name": "Streamlit Chat"})
-    st.session_state.conversation_id = conversation['conversation'].id
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -26,13 +21,14 @@ if query:
         st.markdown(query)
     st.session_state.messages.append({"role": "user", "content": query})
     
-    # Get response from Pieces Copilot
-    response = pieces_client.prompt_conversation(
-        message=query,
-        conversation_id=st.session_state.conversation_id
-    )
-    
+    def get_streaming_response(query):
+        for answer in pieces_client.copilot.stream_question(query):
+            if answer.question:
+                answers = answer.question.answers.iterable
+                for answer in answers:
+                    yield answer.text
     # Display assistant message
     with st.chat_message("assistant"):
-        st.markdown(response['text'])
-    st.session_state.messages.append({"role": "assistant", "content": response['text']})
+        response = st.write_stream(get_streaming_response(query))
+  
+    st.session_state.messages.append({"role": "assistant", "content":response })
